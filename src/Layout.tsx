b2b76ba0601b0f4f79e33d8de4d737a4c44188e5
Volha Linkevich <volha.linkevich@gmail.com>
2023-08-29 10:02:52 +0400

    Add styles for the scrollbar

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,14 +6,16 @@ interface PropsType {
 
 function Layout(props: PropsType) {
     return (
-        <div className="relative w-full min-h-screen bg-stone-50 text-stone-800">
-            <div className="sticky z-50 top-0 bg-stone-50">
+        <div className="relative w-full h-screen overflow-y-hidden bg-stone-50 text-stone-800">
+            <div className="fixed w-full z-50 top-0 bg-stone-50">
                 <div className="rounded-b-sm text-stone-300 bg-stone-800 shadow-md shadow-stone-700">
                     <Header />
                 </div>
             </div>
-            <div className="relative">
-                {props.children}
+            <div className="relative h-screen overflow-y-auto">
+                <div className="mt-16">
+                    {props.children}
+                </div>
             </div>
             <div className="sticky bottom-0 rounded-t-sm bg-stone-50 ">
                 <div className="bg-stone-800 text-stone-300">
